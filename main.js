509ab9836cede235b4472a6fcc1e9829b89843f5
Snehdeep <jammusneh@gmail.com>
2020-06-08 11:15:24 -0400

    Basic Html,js and css file

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var questionNo =1;
+var points = 0;
+var numofQuestion = 5;
+
+function submit(){
+
+     var ans =['A','D','A','B','A','D','B','A','A','C'];
+     
+     var q1 = document.forms['quiz']['q1'].value;
+     var q2 = document.forms['quiz']['q2'].value;
+     var q3 = document.forms['quiz']['q3'].value;
+     var q4 = document.forms['quiz']['q4'].value;
+     var q5 = document.forms['quiz']['q5'].value;
+    
+
+    }
+    
+
